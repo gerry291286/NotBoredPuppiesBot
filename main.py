bea59a5fa32a51562c387384f74f249546f12620
KUMPULAN SCRIPT <71230888+gerry291286@users.noreply.github.com>
2024-11-05 08:08:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import asyncio
 from contextlib import suppress
-
 from bot.utils.launcher import process
 
-
 async def main():
-    await process()
-
+    # Membaca query_id dari file query.txt
+    with open('query.txt', 'r') as file:
+        query_id = file.readline().strip()
+    # Inisialisasi klien Telegram dengan query_id
+    client = TelegramClient('session_name', query_id=query_id)
+    await client.start()
+    await process(client)
 
 if __name__ == '__main__':
     with suppress(KeyboardInterrupt):
